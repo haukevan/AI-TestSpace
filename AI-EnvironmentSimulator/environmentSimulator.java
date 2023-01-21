ed32446d642fc013de1e23d685ead90243f64834
haukevan <haukevan@gmail.com>
2023-01-21 11:09:17 -0800

    create new class

--- AI-EnvironmentSimulator/environmentSimulator.java
+++ AI-EnvironmentSimulator/environmentSimulator.java
@@ -1,5 +1,5 @@
-class subscribe {
-    public static void main(String[] args) {
-        System.out.print("HELLO JAVAJ");
+public class MyClass {
+    public static void main(String args[]) {
+        System.out.println("hello java");
     }
 }
